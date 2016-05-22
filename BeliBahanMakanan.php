18b3be96f3367fa9ef423d7465b218db300f9691
airawati <airawatisurjana@gmail.com>
2016-05-22 19:36:18 +0700

    Update BeliBahanMakanan.php

--- BeliBahanMakanan.php
+++ BeliBahanMakanan.php
@@ -6,7 +6,7 @@
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <style>
 table tr td{padding: 5px;}
-button {
+#button1, button {
     background-color: #101010; 
     border: none;
     color: white;
@@ -40,8 +40,16 @@ select {
   <h2>Foodie - PEMBELIAN BAHAN MAKANAN</h2>
   <hr />   
   <?php
+  if (!isset($_POST["banyak"])) {
    $nomor_nota = $_POST['nomor_nota'];
-   $nama_supplier = $_POST['supplier']
+   $nama_supplier = $_POST['supplier'];
+   $waktuTemp =  new DateTime();
+   $waktu =$waktuTemp->format('Y-m-d H:i:s');
+   $emailStaff = $_SESSION["userlogin"];
+   $_SESSION["nomor_nota"] = $_POST["nomor_nota"];
+   include "../connect.php";
+   $sql2 = pg_query($conn,"INSERT INTO PEMBELIAN VALUES('$nomor_nota','$waktu','$nama_supplier','$emailStaff')"); 
+   }
    /*
    INSERT KE TABEL PEMBELIAN DISINI
    */
@@ -74,10 +82,12 @@ select {
         <th>Total</th>
       </tr>
     </thead>
+     
     <tbody id="wrapper">
+   <form method="POST" action="">
       <tr>
         <td>
-        <select name="bahanbaku1">
+        <select name="bahanbaku0">
         <?php 
                         include "../connect.php";
                         $sql =  pg_query($conn,"SELECT nama FROM Bahan_baku"); 
@@ -87,9 +97,9 @@ select {
         ?>
         </select>
         </td>
-        <td><input class="form-control" onkeyup="calculate_total(0)" id="harga0">
+        <td><input class="form-control" onkeyup="calculate_total(0)" id="harga0" name="harga0">
         <td>
-        <select name="satuan1">
+        <select name="satuan0">
               <?php 
                         include "../connect.php";
                         $sql =  pg_query($conn,"SELECT DISTINCT satuanawal FROM Konversi_bahan_baku "); 
@@ -100,32 +110,52 @@ select {
         </select>
         </td>
         <td>
-          <input class="form-control" onkeyup="calculate_total(0)" id="qty0">
+          <input class="form-control" onkeyup="calculate_total(0)" id="qty0" name="jumlah0">
         </td>
 		<td>
-    <input class="form-control" id="total0" disabled>  
+    <input class="form-control" id="total0" disabled> <input type="number" hidden="hidden" id="banyak" name="banyak" value="1">
+    <input type="text" hidden="hidden" name="nomornota" value='<?php echo $nomor_nota;?>'>
     </td>
       </tr>
     </tbody>
+    
   </table>
-  <button>Simpan</button>
+  <input type="submit" id="button1" value="Simpan">
+  </form>
   </div>
 </div>
-
+<?php
+            if (isset($_POST["banyak"])) {
+                        include "../connect.php";
+                        $banyak = $_POST["banyak"];
+                        $nomor_nota = $_POST["nomornota"];
+                        for($jj=0; $jj<=$banyak; $jj++){
+                            $nama = $_POST["bahanbaku".$jj];
+                            echo $nama;
+                            $harga = $_POST["harga".$jj];
+                            echo $harga;
+                            $satuan = $_POST["satuan".$jj];
+                            echo $satuan;
+                            $jumlah = $_POST["jumlah".$jj];
+                            $sql2 = pg_query($conn,"INSERT INTO PEMBELIAN_BAHAN_BAKU VALUES('$nama', '$nomor_nota','$jumlah','$satuan','$harga')");
+                        }
+                        
+                }
+?>
 </body>
 </html>
 <script>
-var id= 1;
+var id= 0;
 function tambahBahan() {
      var newdiv = document.createElement('tr');
      ++id;
-     newdiv.innerHTML = "<td><select name='bahanbaku"+id+"'><?php include "../connect.php";$sql =  pg_query($conn,"SELECT nama FROM Bahan_baku"); while ($row_nama = pg_fetch_array($sql)) {echo "<option value='".($row_nama['nama'])."'>".($row_nama['nama'])."</option>";}?> </select></td><td><input type='text' name='harga'"+id+"></td><td><select name='satuan"+id+"'><?php include "../connect.php";$sql =  pg_query($conn,"SELECT DISTINCT satuanawal FROM Konversi_bahan_baku "); while ($row_satuan = pg_fetch_array($sql)) {echo "<option value='".($row_satuan['satuanawal'])."'>".($row_satuan['satuanawal'])."</option>";}?></select></td><td><input type='text' name='jumlah"+id+"'></td><td>000000</td>";
+     newdiv.innerHTML = "<td><select name='bahanbaku"+id+"'><?php include "../connect.php";$sql =  pg_query($conn,"SELECT nama FROM Bahan_baku"); while ($row_nama = pg_fetch_array($sql)) {echo "<option value='".($row_nama['nama'])."'>".($row_nama['nama'])."</option>";}?> </select></td><td><input class='form-control' name='harga"+id+"' onkeyup='calculate_total("+id+")' id='harga"+id+"'></td><td><select name='satuan"+id+"'><?php include "../connect.php";$sql =  pg_query($conn,"SELECT DISTINCT satuanawal FROM Konversi_bahan_baku "); while ($row_satuan = pg_fetch_array($sql)) {echo "<option value='".($row_satuan['satuanawal'])."'>".($row_satuan['satuanawal'])."</option>";}?></select></td><td><input class='form-control' name='jumlah"+id+"' onkeyup='calculate_total("+id+")' id='qty"+id+"'></td><td><input class='form-control' id='total"+id+"' disabled></td>";
     document.getElementById("wrapper").appendChild(newdiv);
+    $('#banyak').val(id);
 }
 
 
 function calculate_total(i) {
 		$('#total' + i).val($('#harga' + i).val() * $('#qty' + i).val());
 }
-
 </script>
